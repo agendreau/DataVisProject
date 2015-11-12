64a292b8754b25250231a4d40f8165f14bb5eba6
bencordova <ben.cordova@gmail.com>
2015-11-11 22:33:07 -0700

    Turn DataSet2 img entries into arrays

--- data/js/data.js
+++ data/js/data.js
@@ -86,28 +86,28 @@ data = {
                "name"      : "Task 2",
                "question"  : "For each country, plot the value of income per capita, value of life exptectancy, and change in population over time",
                "query"     : ["for", "country", "Value of", "income per capita", "life expectancy", "population", "over", "time"],
-               "img"       : {
+               "img"       : [{
                                  "src" : "bubblechart.jpg", //"3Dbarchart.jpg", "bulletchart.png",
                                  "alt" : "Bubble chart showing income per capita, life expectancy, and population over time by country",
-                             }
+                             }]
             },
             {
                "name"      : "Task 2",
                "question"  : "What is the statistical distribution of income per capita over time",
                "query"     : ["statistical","distribution","income per capita","over","time"],
-               "img"       : {
+               "img"       : [{
                                  "src" : "boxplot.png",
                                  "alt" : "Box and whisker plot showing income per capita over time",
-                             }
+                             }]
             },
             {
                "name"      : "Task 2",
                "question"  : "Life expectancy for United States, Canada, Mexico over time",
                "query"     : ["life expectancy", "country", "over", "time"],
-               "img"       : {
+               "img"       : [{
                                  "src" : "linechart.png",
                                  "alt" : "Line chart depicting life expectancy for United States, Canada, Mexico over time",
-                             }
+                             }]
             }
 
             //, ...
